78e12f51014fc20fa54b8995bf5e85f2b48b7044
Chan, Danny <danny50610@yahoo.com.tw>
2017-01-25 01:58:41 +0800

    Fix bug (http://www.minecraftforge.net/forum/index.php?topic=44481.0)

--- src/main/java/com/ImmortalObjectTip/ModInformation.java
+++ src/main/java/com/ImmortalObjectTip/ModInformation.java
@@ -3,5 +3,5 @@ package com.ImmortalObjectTip;
 public class ModInformation {
     public static final String MOD_ID = "immortalobjecttip";
     public static final String MOD_NAME = "Immortal Object Tip";
-    public static final String DEPENDENCIES = "required-after:Forge@[#DEPENDENCIES#,)";
+    public static final String DEPENDENCIES = "required-after:forge@[#DEPENDENCIES#,)";
 }
