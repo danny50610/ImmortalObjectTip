61b0ab579a789d9d0cb8ab3d96587afb44dc4047
Chan, Danny <danny60072@gmail.com>
2019-11-25 21:41:22 +0800

    Fix bug

--- src/main/java/com/ImmortalObjectTip/handler/clinet/RenderTipHandler.java
+++ src/main/java/com/ImmortalObjectTip/handler/clinet/RenderTipHandler.java
@@ -8,9 +8,9 @@ import com.google.common.collect.Lists;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.client.renderer.VertexBuffer;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.SoundEvents;
@@ -101,7 +101,7 @@ public class RenderTipHandler {
     // Knowledge: http://www.minecraftforge.net/forum/index.php?topic=41475.0
     private void renderTipBlock(EntityPlayer player, RenderWorldLastEvent event) {
         Tessellator tessellator = Tessellator.getInstance();
-        VertexBuffer vertexBuffer = tessellator.getBuffer();
+        BufferBuilder vertexBuffer = tessellator.getBuffer();
 
         for (Iterator<TipInfoBlock> it = tipBlockList.iterator(); it.hasNext(); ) {
             TipInfoBlock tip = it.next();
@@ -210,7 +210,7 @@ public class RenderTipHandler {
 
     private void renderTipEntity(EntityPlayer player, RenderWorldLastEvent event) {
         Tessellator tessellator = Tessellator.getInstance();
-        VertexBuffer vertexBuffer = Tessellator.getInstance().getBuffer();
+        BufferBuilder vertexBuffer = Tessellator.getInstance().getBuffer();
 
         for (Iterator<TipInfoEntity> it = tipPlayerList.iterator(); it.hasNext(); ) {
             TipInfoEntity tip = it.next();
